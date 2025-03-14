fbe30a8755325ef90217ce10864027484d6f0f56
Aliaksei (Alexey) Lomats <alexeyttdeveloper@gmail.com>
2025-03-14 18:06:11 +0100

    T2: Added basic packages to proper and fast work: dbal, doctrine, ORM, migrations, monolog, uid, validators and maker-bundle. Added entities and repositories for it: WorkTime and Employee.

---
+++ src/Repository/WorkTimeRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\WorkTime;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<WorkTime>
+ */
+class WorkTimeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, WorkTime::class);
+    }
+}
